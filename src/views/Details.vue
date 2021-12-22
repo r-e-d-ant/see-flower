fc05e0e8d869dddd8c9dbf1d70afbba1f5184be8
r-e-d-ant <mugishathierry65@gmail.com>
2021-12-22 17:06:16 +0200

    product review and add to cart builded

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,30 +1,53 @@
 
 <template>
     <section class="section section-one-to-cart to-cart-product-infos-container" v-if="oneProduct">
-            <!-- <div class="to-cart-product-img"></div> -->
-            <img :src="oneProduct.image" :alt="oneProduct.name" class="to-cart-img">
-            <div class="to-cart-product-infos">
-                <h2 class="to-cart-product-title">{{ oneProduct.name }}</h2>
-                <p class="to-cart-product-description">
-                    {{ oneProduct.description }}
-                </p>
-                <button class="add-to-cart-btn cart-btns">Add To Cart</button>
-            </div>
-        </section>
+        <img :src="oneProduct.image" :alt="oneProduct.name" class="to-cart-img">
+        <div class="to-cart-product-infos">
+            <h2 class="to-cart-product-title">{{ oneProduct.name }}</h2>
+            <p class="to-cart-product-description">{{ oneProduct.description }}</p>
+            <button class="add-to-cart-btn cart-btns">Add To Cart</button>
+        </div>
+    </section>
+    
+    <div class="section-two-to-cart">
+        <div class="nav-buttons">
+            <button class="go-to_reviews-btn goto-btns" ref="reviews" @click="showReviewsComp">Reviews</button>
+            <button class="go-to_make_review-btn goto-btns" ref="makeReview" @click="showMakeReviewComp">Make a Review</button>
+        </div>
+        <userReview v-if="showReviews" @click="showReviewsComp"></userReview>
+        <makeReview v-if="showMakeReview" @click="showMakeReviewComp"></makeReview>
+    </div>
 </template>
 
 <script>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import getProducts from "../firebase"
+
+import userReview from "../components/userReview"
+import makeReview from "../components/makeReview"
 
 export default {
     props: ['id', 'category'],
+    components: { userReview, makeReview },
     setup(props) {
+
+        const showMakeReview = ref(false)
+        const showReviews = ref(true)
+
+        const showMakeReviewComp = () => {
+            showMakeReview.value = true
+            showReviews.value = false
+        }
+        const showReviewsComp = () => {
+            showMakeReview.value = false
+            showReviews.value = true
+        }
+
         const { getOneProduct, oneProduct } = getProducts()
         onMounted(() => {
             getOneProduct(props.category, props.id)
         })
-        return { oneProduct }
+        return { oneProduct, showMakeReviewComp, showMakeReview, showReviewsComp, showReviews }
     },
 }
 </script>
@@ -87,4 +110,39 @@ export default {
     }
 }
 
+/* -- nav buttons -- */
+.nav-buttons {
+    display: flex;
+    position: relative;
+    padding-top: .1rem;
+}
+.nav-buttons::before {
+    content: "";
+    position: absolute;
+    background-color: hsla(21, 72%, 7%, 17%);
+    top: 0;
+    height: .1rem;
+    width: 100%;
+}
+.goto-btns, .submit-review-btn {
+    cursor: pointer;
+    border: none;
+    padding: .9rem 0;
+    font-size: 1rem;
+    font-weight: 500;
+    width: 9rem;
+}
+.go-to_reviews-btn {
+    border-bottom-left-radius: 5px;
+    color: var(--primary-clr);
+    background-color: var(--second-clr);
+}
+.go-to_make_review-btn {
+    border-bottom-right-radius: 5px;
+    background-color: var(--second-clr-alt);
+    transition: 150ms background-color;
+}
+.go-to_make_review-btn:hover {
+    background-color: hsl(21, 74%, 85%);
+}
 </style>
